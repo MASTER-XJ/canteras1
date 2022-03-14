516ceb3e986a4967bd84fbf7de99178cb77c6a3d
MASTER-XJ <62977744+MASTER-XJ@users.noreply.github.com>
2022-03-13 23:29:20 -0500

    Al proyecto hoja de vida, se implementaron cambios usando lenguaje Javascript, como ocutar texto y cambiar de color el fondo de la pagina, segun lo requerido por el coach.

---
+++ html/js/javascript.js
@@ -0,0 +1,101 @@
+function colorChanger(){
+    var selectValue=document.getElementById("Opcion").value;
+    document.getElementById('dummy').innerHTML;
+    document.querySelector('.container-body').style.backgroundColor= selectValue;
+}
+
+function mostrar( ){
+    document.getElementById('exp1').style.display='block';
+}
+
+function ocultar( ){
+    document.getElementById('exp1').style.display='none';
+}
+
+function mostrar2( ){
+    document.getElementById('exp2').style.display='block';
+}
+
+function ocultar2( ){
+    document.getElementById('exp2').style.display='none';
+}
+
+function mostrar3( ){
+    document.getElementById('exp3').style.display='block';
+}
+
+function ocultar3( ){
+    document.getElementById('exp3').style.display='none';
+}
+
+function mostrar4( ){
+    document.getElementById('exp4').style.display='block';
+}
+
+function ocultar4( ){
+    document.getElementById('exp4').style.display='none';
+}
+
+function mostrarref1( ){
+    document.getElementById('ref1').style.display='block';
+}
+
+function ocultarref1( ){
+    document.getElementById('ref1').style.display='none';
+}
+
+function mostrarref2( ){
+    document.getElementById('ref2').style.display='block';
+}
+
+function ocultarref2( ){
+    document.getElementById('ref2').style.display='none';
+}
+
+function mostrarref3( ){
+    document.getElementById('ref3').style.display='block';
+}
+
+function ocultarref3( ){
+    document.getElementById('ref3').style.display='none';
+}
+
+function mostrarfam1( ){
+    document.getElementById('fam1').style.display='block';
+}
+
+function ocultarfam1( ){
+    document.getElementById('fam1').style.display='none';
+}
+
+function mostrarfam2( ){
+    document.getElementById('fam2').style.display='block';
+}
+
+function ocultarfam2( ){
+    document.getElementById('fam2').style.display='none';
+}
+
+function mostrarfam3( ){
+    document.getElementById('fam3').style.display='block';
+}
+
+function ocultarfam3( ){
+    document.getElementById('fam3').style.display='none';
+}
+
+function mostrarpro( ){
+    document.getElementById('pro').style.display='block';
+}
+
+function ocultarpro( ){
+    document.getElementById('pro').style.display='none';
+}
+
+function mostrartec( ){
+    document.getElementById('tec').style.display='block';
+}
+
+function ocultartec( ){
+    document.getElementById('tec').style.display='none';
+}
